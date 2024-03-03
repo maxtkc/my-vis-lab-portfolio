018e828998efe9cd9a938a7c643e7154271d76d2
Max Katz-Christy <maxkatzchristy@gmail.com>
2024-03-03 13:33:46 -0500

    migrate old site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Max Katz-Christy: Personal site and portfolio</title>
+</svelte:head>
+<h1>Max Katz-Christy</h1>
+<p>Hi I like bikes. I like trains. I like learning new sports. Recently I got into bike polo, mountain bike racing, and ice hockey. These are all quite enjoyable.</p>
+<img src="images/cat.webp" alt="cat" />
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+    <title>Contact me</title>
+</svelte:head>
+<h1>Contact</h1>
+<form action="mailto:junk@gmail.com" method="POST" enctype="text/plain">
+    <label for="email">Email: <input name="email" id="name" type="email"></label>
+    <label for="subject">Subject: <input name="subject" type="text"></label>
+    <label for="body">Body: <textarea name="body"></textarea></label>
+    <button>Send Email</button>
+</form>
