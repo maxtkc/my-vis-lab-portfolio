706314899950c335556fe80a6b16314f986bca47
Max Katz-Christy <maxkatzchristy@gmail.com>
2024-03-03 14:17:06 -0500

    make project separate

---
+++ src/lib/Project.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let info = {};
+    export let hLevel = 2;
+    let p = info;
+</script>
+<style>
+article {
+    display: grid;
+    grid-template-rows: subgrid;
+    grid-row: span 3;
+}
+
+article h2, h3 {
+    margin: 0;
+}
+</style>
+<article>
+    <svelte:element this={"h" + hLevel}>{ p.title }</svelte:element>
+    <img src="{p.image}" alt="">
+    <p>{p.description}</p>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,18 @@
 <svelte:head>
     <title>Max Katz-Christy: Personal site and portfolio</title>
 </svelte:head>
+
+<script>
+import projects from '$lib/projects.json';
+import Project from "$lib/Project.svelte";
+</script>
+
 <h1>Max Katz-Christy</h1>
 <p>Hi I like bikes. I like trains. I like learning new sports. Recently I got into bike polo, mountain bike racing, and ice hockey. These are all quite enjoyable.</p>
-<img src="images/cat.webp" alt="cat" />
+<img width="50%" src="images/cat.webp" alt="cat" />
+<h2>Latest Projects</h2>
+<div class="projects">
+    {#each projects.slice(0, 3) as p}
+        <Project info={p} hLevel=3 />
+    {/each}
+</div>
