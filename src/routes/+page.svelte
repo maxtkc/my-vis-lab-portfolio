cb6a46d602d12944bc17ad58a31770aaf61f75ae
Max Katz-Christy <maxkatzchristy@gmail.com>
2024-03-11 11:29:36 -0400

    finish lab 4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,40 @@ import Project from "$lib/Project.svelte";
 
 <h1>Max Katz-Christy</h1>
 <p>Hi I like bikes. I like trains. I like learning new sports. Recently I got into bike polo, mountain bike racing, and ice hockey. These are all quite enjoyable.</p>
+
+{#await fetch("https://api.github.com/users/maxtkc") }
+	<p>Loading...</p>
+{:then response}
+	{#await response.json()}
+		<p>Decoding...</p>
+	{:then data}
+                <section>
+                <h2>My GitHub Stats</h2>
+                <dl>
+                    <dt>Followers</dt>
+                    <dd>{ data.followers }</dd>
+
+                    <dt>Following</dt>
+                    <dd>{ data.following }</dd>
+
+                    <dt>Public Repos</dt>
+                    <dd>{ data.public_repos }</dd>
+
+                    <dt>Public Gists</dt>
+                    <dd>{ data.public_gists }</dd>
+                </dl>
+                </section>
+	{:catch error}
+		<p class="error">
+			Something went wrong: {error.message}
+		</p>
+	{/await}
+{:catch error}
+	<p class="error">
+		Something went wrong: {error.message}
+	</p>
+{/await}
+
 <img width="50%" src="images/cat.webp" alt="cat" />
 <h2>Latest Projects</h2>
 <div class="projects">
