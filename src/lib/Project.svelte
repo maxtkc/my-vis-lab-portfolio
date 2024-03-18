7660ff4853b5d5f866ffa4791fac3e47b6a3c472
Max Katz-Christy <maxkatzchristy@gmail.com>
2024-03-18 13:37:37 -0400

    start lab 6

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -13,9 +13,17 @@ article {
 article h2, h3 {
     margin: 0;
 }
+
+p:nth-child(2) {
+      font-family: Baskerville;
+      font-variant-numeric: oldstyle-nums;
+}
 </style>
 <article>
     <svelte:element this={"h" + hLevel}>{ p.title }</svelte:element>
     <img src="{p.image}" alt="">
-    <p>{p.description}</p>
+    <div>
+        <p>{p.description}</p>
+        <p>C. {p.year}</p>
+    </div>
 </article>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,8 +5,10 @@
 <script>
 import projects from '$lib/projects.json';
 import Project from "$lib/Project.svelte";
+import Pie from "$lib/Pie.svelte";
 </script>
 <h1>{ projects.length } Projects</h1>
+<Pie />
 <div class="projects">
     {#each projects as p}
         <Project info={p} />
