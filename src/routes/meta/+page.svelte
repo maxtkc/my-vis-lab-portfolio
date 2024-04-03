22ab2aa40f45171863afd1b7cb5a62e3231bbe37
Max Katz-Christy <maxkatzchristy@gmail.com>
2024-04-03 15:16:36 -0400

    add commits stats

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -1,6 +1,158 @@
+<script>
+  import * as d3 from "d3";
+
+  import { onMount } from "svelte";
+
+  let data = [];
+  let commits = [];
+
+  onMount(async () => {
+    data = await d3.csv("loc.csv", row => ({
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + "T00:00" + row.timezone),
+      datetime: new Date(row.datetime)
+    }));
+    commits = d3.groups(data, d => d.commit).map(([commit, lines]) => {
+      let first = lines[0];
+      let {author, date, time, timezone, datetime} = first;
+      let ret = {
+        id: commit,
+        url: "https://github.com/vis-society/lab-7/commit/" + commit,
+        author, date, time, timezone, datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length
+      };
+
+      // Like ret.lines = lines
+      // but non-enumerable so it doesn’t show up in JSON.stringify
+      Object.defineProperty(ret, "lines", {
+        value: lines,
+        configurable: true,
+        writable: true,
+        enumerable: false,
+      });
+
+      return ret;
+    });
+  });
+
+  let width = 1000, height = 600;
+  let margin = {top: 10, right: 10, bottom: 30, left: 20};
+
+  let usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left
+  };
+  usableArea.width = usableArea.right - usableArea.left;
+  usableArea.height = usableArea.bottom - usableArea.top;
+
+  let xScale, yScale;
+  $: xScale = d3.scaleTime(d3.extent(commits, commit => commit.date), [usableArea.left, usableArea.right]).nice();
+  $: yScale = d3.scaleLinear([0, 24], [usableArea.bottom, usableArea.top])
+
+  let xAxis, yAxis;
+
+  $: {
+    d3.select(xAxis).call(d3.axisBottom(xScale));
+    d3.select(yAxis).call(d3.axisLeft(yScale));
+    d3.select(yAxis).call(d3.axisLeft(yScale).tickFormat(d => String(d %
+      24).padStart(2, "0") + ":00"));
+  }
+
+  let yAxisGridlines;
+
+  $: {
+    d3.select(yAxisGridlines).call(
+      d3.axisLeft(yScale)
+      .tickFormat("")
+      .tickSize(-usableArea.width)
+    );
+  }
+
+  let hoveredIndex = -1;
+  $: hoveredCommit = commits[hoveredIndex] ?? {};
+
+</script>
+
+<style>
+  svg {
+    overflow: visible;
+  }
+  .gridlines {
+    stroke-opacity: .2;
+  }
+  circle {
+    transition: 200ms;
+    transform-origin: center;
+    transform-box: fill-box;
+    &:hover {
+      transform: scale(1.5);
+    }
+  }
+  dl.info {
+    display:grid;
+    grid-template-columns: 50px 50px;
+  }
+  .tooltip {
+    position: fixed;
+    top: 1em;
+    left: 1em;
+  }
+</style>
+
 <svelte:head>
   <title>Meta</title>
 </svelte:head>
 
 <h1>Meta</h1>
-hello
+<dl class="stats">
+	<dt>Total <abbr title="Lines of code">LOC</abbr></dt>
+	<dd>{data.length}</dd>
+
+	<dt>Total number of commits</dt>
+	<dd>{commits.length}</dd>
+
+	<dt>Number of files in the codebase</dt>
+  <dd>{d3.group(data, d => d.file).size}</dd>
+
+	<dt>Maximum file length (in lines)</dt>
+  <dd>{d3.max(data, d => d.line)}</dd>
+
+	<dt>Longest file</dt>
+  <dd>{d3.greatest(data, d => d.line)?.file}</dd>
+</dl>
+
+<h2>Commits by time of day</h2>
+
+<dl id="commit-tooltip" class="info tooltip">
+	<dt>Commit</dt>
+	<dd><a href="{ hoveredCommit.url }" target="_blank">{ hoveredCommit.id }</a></dd>
+
+	<dt>Date</dt>
+	<dd>{ hoveredCommit.datetime?.toLocaleString("en", {date: "full"}) }</dd>
+
+	<!-- Add: Time, author, lines edited -->
+</dl>
+
+<svg viewBox="0 0 {width} {height}">
+  <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
+  <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
+  <g class="gridlines" transform="translate({usableArea.left}, 0)" bind:this={yAxisGridlines} />
+  <g class="dots">
+  {#each commits as commit, index }
+    <circle
+      cx={ xScale(commit.datetime) }
+      cy={ yScale(commit.hourFrac) }
+      r="5"
+      fill="steelblue"
+      on:mouseenter={evt => hoveredIndex = index}
+      on:mouseleave={evt => hoveredIndex = -1}
+      />
+  {/each}
+  </g>
+</svg>
