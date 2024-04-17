1dbfa3c5055b4052182338e52948db54372b4d76
Max Katz-Christy <maxkatzchristy@gmail.com>
2024-04-17 12:40:15 -0400

    2.3

--- src/routes/meta/FileLines.svelte
+++ src/routes/meta/FileLines.svelte
@@ -8,6 +8,7 @@
     files = d3.groups(lines, d => d.file).map(([name, lines]) => {
       return {name, lines};
     });
+    files = d3.sort(files, d => -d.lines.length);
   }
 </script>
 
@@ -22,16 +23,34 @@
   }
   dd {
     grid-column: 2;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: start;
+    align-content: start;
+    gap: .15em;
+    padding-top: .6em;
+    margin-left: 0;
+  }
+  .line {
+    display: flex;
+    width: .5em;
+    aspect-ratio: 1;
+    background: steelblue;
+    border-radius: 50%;
   }
 </style>
 
 <dl class="files">
-	{#each files as file (file.name) }
-		<div>
-			<dt>
-				<code>{file.name}</code>
-			</dt>
-			<dd>{file.lines.length} lines</dd>
-		</div>
-	{/each}
+  {#each files as file (file.name) }
+    <div>
+      <dt>
+      <code>{file.name}</code>
+      </dt>
+      <dd>
+        {#each file.lines as line (line.line) }
+          <div class="line"></div>
+        {/each}
+      </dd>
+    </div>
+  {/each}
 </dl>
